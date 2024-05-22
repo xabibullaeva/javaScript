0bd25e965a28e5ec0b01bb019157eda85d022137
-g <-g>
2024-05-22 23:19:34 +0500

    javaScript

---
+++ java.js
@@ -0,0 +1,37 @@
+var name = prompt("Enter your name");
+console.log("Your name is " + name);
+
+var age = prompt("Enter your age");
+console.log("Your age is " + age);
+
+var task1 = +prompt("Solve question: 9+13=?")
+console.log("Your answer is " + task1)
+
+var task2 = +prompt("Solve question: 56-14=?")
+console.log("Your answer is " + task2)
+
+var task3 = +prompt("Solve question: 5*5=?")
+console.log("Your answer is " + task3)
+
+var task4 = +prompt("Solve question: 81/9=?")
+console.log("Your answer is " + task4)
+
+var task5 = +prompt("Solve question: 15%7=?")
+console.log("Your answer is " + task5)
+
+var task6 = prompt("Solve question: 58==58 =?(true/false)")
+console.log("Your answer is " + task6)
+
+var task7 = prompt("Solve question: 19!=29 =?(true/false)")
+console.log("Your answer is " + task7)
+
+var task8 = prompt("Solve question: 36>15 =?(true/false)")
+console.log("Your answer is " + task8)
+
+var task9 = prompt("Solve question: 48<45 =?(true/false)")
+console.log("Your answer is " + task9)
+
+var task10 = prompt("Solve question: 48>=23(true/false)")
+console.log("Your answer is " + task10)
+
+alert("Done!")
